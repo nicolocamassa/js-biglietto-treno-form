422717b984031513ed96888174e082420e586ed7
nicolocamassa <camassanicolo03@gmail.com>
2023-11-21 16:27:34 +0100

    [ADD] forms connected to js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
-/* Dati in input */
-let km = prompt('Quanti km');
-let age = prompt('Quanti anni');
+let button = document.getElementById('submitBtn');
 
-/* Inizializzazione dello sconto e del prezzo standard per km */
-let discount;
-const priceKm = 0.21;
+button.addEventListener('click', function () {
+    /* Dati in input */
+    let age = document.getElementById('age').value;
+    let km = document.getElementById('distance').value;
 
-/* Seleziona lo sconto basandosi sull'età */
-if(age < 18){
-    discount = 20;
-}else if(age > 65){
-    discount = 40;
-}else{
-    discount = 0
-}
+    /* Inizializzazione dello sconto e del prezzo standard per km */
+    let discount;
+    const priceKm = 0.21;
 
-/* Calcolo del prezzo finale */
-let discountCalc = ((km * priceKm) * discount) / 100;
-let totalDiscount =  (km * priceKm) - discountCalc;
+    /* Seleziona lo sconto basandosi sull'età */
+    if (age < 18) {
+        discount = 20;
+    } else if (age > 65) {
+        discount = 40;
+    } else {
+        discount = 0
+    }
 
-/* Output */
-console.log(totalDiscount.toFixed(2));
+    /* Calcolo del prezzo finale */
+    let discountCalc = ((km * priceKm) * discount) / 100;
+    let totalDiscount = (km * priceKm) - discountCalc;
+
+    /* Output */
+    document.getElementById('price').innerHTML = totalDiscount.toFixed(2);
+})
+
+
+
+
+
+
