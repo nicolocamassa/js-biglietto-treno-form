d859e947e6831fc792c7ce25a39e077db289f7bc
nicolocamassa <camassanicolo03@gmail.com>
2023-11-21 16:09:56 +0100

    [ADD] total price calculation added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
-let km = promp('Quanti km');
-let age = promp('Quanti anni');
+/* Dati in input */
+let km = prompt('Quanti km');
+let age = prompt('Quanti anni');
 
+/* Inizializzazione dello sconto e del prezzo standard per km */
+let discount;
 const priceKm = 0.21;
-let ticketPrice = km * priceKm;
 
+/* Seleziona lo sconto basandosi sull'età */
+if(age < 18){
+    discount = 20;
+}else if(age > 65){
+    discount = 40;
+}else{
+    discount = 0
+}
+
+/* Calcolo del prezzo finale */
+let discountCalc = ((km * priceKm) * discount) / 100;
+let totalDiscount =  (km * priceKm) - discountCalc;
+
+/* Output */
+console.log(totalDiscount.toFixed(2));
